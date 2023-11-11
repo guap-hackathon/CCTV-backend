006967f9fd8018c8873690db1fdcaaea9ab94dfe
Yan <matusyan24@gmail.com>
2023-11-11 11:34:58 +0300

    init

---
+++ CCTV-backend/Services/StreamService.cs
@@ -0,0 +1,14 @@
+namespace CCTV_backend.Services;
+
+public class StreamService : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
